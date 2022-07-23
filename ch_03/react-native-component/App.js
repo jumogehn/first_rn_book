8d54840c606904d082954e257d6d767099639ea1
jumogehn <jumogehn@gmail.com>
2022-07-23 20:45:24 +0900

    ch03:if statement

--- ch_03/react-native-component/App.js
+++ ch_03/react-native-component/App.js
@@ -6,7 +6,13 @@ export default function App() {
   const name = 'jumogehn';
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is {name}</Text>
+      <Text style={styles.text}>
+          {(() => {
+            if (name === 'Hanbit') return 'My name is Hanbit';
+            else if (name === 'jumogehn') return 'My name is jumogehn';
+            else return 'My name is React Native';
+          })()}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
