2a8319a1772c79dba10ed1cdec4d20e43e7c7606
jumogehn <jumogehn@gmail.com>
2022-07-23 18:23:11 +0900

    ch03:short usage of Fragment

--- ch_03/react-native-component/App.js
+++ ch_03/react-native-component/App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Text } from 'react-native';
 
 export default function App() {
   return (
-    <Fragment>
+    <>
       <Text>Hi! React Native!</Text>
       <StatusBar style="auto" />
-    </Fragment>
+    </>
   );
 }
