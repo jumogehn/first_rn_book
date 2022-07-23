39545548b8739ae8971263d178eac374dc11cfe4
jumogehn <jumogehn@gmail.com>
2022-07-23 19:01:14 +0900

    ch03:JSX uses javascript variable

--- ch_03/react-native-component/App.js
+++ ch_03/react-native-component/App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = 'jumogehn';
   return (
-    <>
-      <Text>Hi! React Native!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>My name is {name}</Text>
       <StatusBar style="auto" />
-    </>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
+
