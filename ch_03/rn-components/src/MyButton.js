b3e7d7adb0502abb4a8839f8edf53e791c6e1bea
jumogehn <jumogehn@gmail.com>
2022-07-24 20:48:47 +0900

    ch03: propTypes

--- ch_03/rn-components/src/MyButton.js
+++ ch_03/rn-components/src/MyButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {TouchableOpacity, View, Text} from 'react-native';
+import PropTypes from 'prop-types';
 
 const MyButton = ({title, onPress, children}) => {
   //console.log(props);
@@ -17,6 +18,11 @@ const MyButton = ({title, onPress, children}) => {
 MyButton.defaultProps = {
   title: 'default',
   onPress: () => alert('default')
+}
+
+MyButton.propTypes = {
+  title: PropTypes.string,
+  onPress: PropTypes.func
 }
 
 export default MyButton;
