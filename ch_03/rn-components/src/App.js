e499387a9d9f185d2f888277adf6ebd5028a1c70
jumogehn <jumogehn@gmail.com>
2022-07-24 20:35:48 +0900

    ch03: default props 1

--- ch_03/rn-components/src/App.js
+++ ch_03/rn-components/src/App.js
@@ -5,7 +5,7 @@ import MyButton from './MyButton';
 export default function App() {
   return (
     <View style={styles.container}>
-      <MyButton title='My Button 1' onPress={() => alert('1')}/>
+      <MyButton />
       <MyButton title='My Button 2' onPress={() => alert('2')}>My Button 3</MyButton>
       <MyButton onPress={() => alert('3')}>Good</MyButton>
       <StatusBar style="auto" />
--- ch_03/rn-components/src/MyButton.js
+++ ch_03/rn-components/src/MyButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {TouchableOpacity, View, Text} from 'react-native';
 
-const MyButton = (props) => {
-  console.log(props);
+const MyButton = ({title = 'title', onPress = () => {}, children}) => {
+  //console.log(props);
   return (
-          <TouchableOpacity onPress={props.onPress}>
+          <TouchableOpacity onPress={onPress}>
           <View style={{backgroundColor:'blue', pading: 10, margin:10}}>
           <Text style={{fontSize:20, color:'white'}}>
-            {props.children || props.title}
+            {children || title}
           </Text>
           </View>
           </TouchableOpacity>
