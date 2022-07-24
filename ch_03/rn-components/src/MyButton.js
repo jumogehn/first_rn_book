3abc059dad76c76aa1ea1347cea0726f1e00ebe3
jumogehn <jumogehn@gmail.com>
2022-07-24 15:41:50 +0900

    ch03: MyButton - hitSlop, pressRetensionOffset

--- ch_03/rn-components/src/MyButton.js
+++ ch_03/rn-components/src/MyButton.js
@@ -3,8 +3,11 @@ import {TouchableOpacity, View, Text} from 'react-native';
 
 const MyButton = () => {
   return (
-          <TouchableOpacity onPress={() => alert('My Button')}>
-          <View style={{backgroundColor:'blue', pading: 10}}>
+          <TouchableOpacity
+            onPress={() => alert('My Button')}
+            //hitSlop={{left:100, top:100, right:100, bottom:100}}>
+            pressRetensionOffset={{left:10, top:10, right:10, bottom:10}}>
+          <View style={{backgroundColor:'blue', pading: 10, margin:10}}>
           <Text style={{fontSize:20, color:'white'}}>
           MyButton
           </Text>
