ad38f4af46cea797d84cb8c89efc1164a13c928a
jumogehn <jumogehn@gmail.com>
2022-08-21 15:26:38 +0900

    styled component implemented

---
+++ ch_04/styled_component/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import Button from './components/Button';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  aligh-items: center;
+  justify-content: center;
+`;
+
+const App = () => {
+  return (
+          <Container>
+            <Button title="Hanbit" />
+            <Button title="React Native" />
+          </Container>
+
+  );
+};
+
+export default App;
+
