b270d6b75bbe0409d4cf8d03529c65ca756cde7a
jumogehn <jumogehn@gmail.com>
2022-09-12 17:25:14 +0900

    added Input component

---
+++ ch_04/styled_component/src/components/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const StyledInput = styled.TextInput`
+ width: 200px;
+ height: 60px;
+ margin: 5px;
+ padding: 10px;
+ border-radius: 10px;
+ border: 2px;
+ border-color: #3498db;
+ font-size: 24px;
+`;
+
+const Input = () => {
+  return <StyledInput placeholder="Enter a Text..."
+          placeholderTextColor="#3498db" />;
+};
+
+export default Input;
